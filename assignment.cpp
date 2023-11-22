f0a0537277c0f4230003d47ef3cbda14f605e930
ghana01 <151663771+ghana01@users.noreply.github.com>
2023-11-22 20:28:38 +0530

    solved the assigment 2 of week 1

---
+++ assignment.cpp
@@ -0,0 +1,12 @@
+//5.WAP to find the difference between ASCII of two characters ,take them as input
+#include<iostream>
+using namespace std;
+int main(){
+    char a,b;
+    int diff;
+    cout<<"enter the two character"<<endl;
+    cin>>a>>b;
+    diff = b-a;
+    cout<<diff;
+
+}
